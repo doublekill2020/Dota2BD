0906562e27f70315659bdc877dec878238604e5d
sh1tge <sh1tge@bfea13ba-dd65-4c7e-bb19-bbca4929ae71>
2014-10-25 12:09:01 +0000

    还缺一个beans 有空在搞 没测试 模拟器太陡壁

---
+++ MyDotaBuff/src/cn/edu/mydotabuff/bean/MacthStatistics.java
@@ -0,0 +1,54 @@
+package cn.edu.mydotabuff.bean;
+
+public class MacthStatistics {
+
+	//全部比赛比赛: 231胜率: 48.48%KDA: 2.4
+
+	private String Type;
+	private String PlayTimes;
+	private String Winning;
+	private String KAD;
+	public String getType() {
+		return Type;
+	}
+	public void setType(String type) {
+		Type = type;
+	}
+	public String getPlayTimes() {
+		return PlayTimes;
+	}
+	public void setPlayTimes(String playTimes) {
+		PlayTimes = playTimes;
+	}
+	public String getWinning() {
+		return Winning;
+	}
+	public void setWinning(String winning) {
+		Winning = winning;
+	}
+	public String getKAD() {
+		return KAD;
+	}
+	public void setKAD(String kAD) {
+		KAD = kAD;
+	}
+	@Override
+	public String toString() {
+		return "MacthStatistics [Type=" + Type + ", PlayTimes=" + PlayTimes
+				+ ", Winning=" + Winning + ", KAD=" + KAD + "]";
+	}
+	public MacthStatistics() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public MacthStatistics(String type, String playTimes, String winning,
+			String kAD) {
+		super();
+		Type = type;
+		PlayTimes = playTimes;
+		Winning = winning;
+		KAD = kAD;
+	}
+	
+	
+}
