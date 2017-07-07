65c85835b51d992aaa634801a437d8df6021789b
sadhu <yaocai321>
2017-07-07 10:55:34 +0800

    add getRealm method

--- MyDotaBuff/app/src/main/java/cn/edu/mydotabuff/ui/presenter/impl/MainPresenterImpl.java
+++ MyDotaBuff/app/src/main/java/cn/edu/mydotabuff/ui/presenter/impl/MainPresenterImpl.java
@@ -1,19 +1,17 @@
 package cn.edu.mydotabuff.ui.presenter.impl;
 
-import java.util.List;
-
+import cn.edu.mydotabuff.base.BasePresenterImpl;
 import cn.edu.mydotabuff.ui.presenter.IMainPresenter;
+import cn.edu.mydotabuff.ui.view.IMainView;
 
 /**
  * Created by nevermore on 2017/7/6 0006.
  */
 
-public class MainPresenterImpl implements IMainPresenter {
-    @Override
-    public void onDestroy() {
-
+public class MainPresenterImpl extends BasePresenterImpl<IMainView> implements IMainPresenter {
+    public MainPresenterImpl(IMainView view) {
+        super(view);
     }
-
     @Override
     public void doSyncMatch() {
 
