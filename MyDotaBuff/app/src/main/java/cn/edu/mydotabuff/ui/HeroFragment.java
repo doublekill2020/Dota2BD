5490d830eb15317673420ce21e30d9b31e964796
tinker <1006401052yh@gmail.com>
2017-07-12 00:23:14 +0800

    PlayerDetailActivity逻辑

---
+++ MyDotaBuff/app/src/main/java/cn/edu/mydotabuff/ui/HeroFragment.java
@@ -0,0 +1,10 @@
+package cn.edu.mydotabuff.ui;
+
+import cn.edu.mydotabuff.base.BaseFragment;
+
+/**
+ * Created by nevermore on 2017/7/11 0011.
+ */
+
+public class HeroFragment extends BaseFragment {
+}
